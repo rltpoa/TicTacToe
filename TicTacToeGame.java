7638b9c70a851be7f1b2e5a385f6978aab53e466
rlt <rltpoa@users.noreply.github.com>
2022-12-16 01:22:37 -0300

    inicia sempre por X e reseta estado sem reiniciar

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,14 +4,13 @@ import java.util.*;
 import javax.swing.*;
 
 /**
- *  copiado de: 
- *  https://www.codespeedy.com/tic-tac-toe-game-using-java-swing-in-java/
+ * copiado de:
+ * https://www.codespeedy.com/tic-tac-toe-game-using-java-swing-in-java/
  * 
  **/
 
 public class TicTacToeGame implements ActionListener {
 
-  
     JFrame frame = new JFrame();
     JPanel t_panel = new JPanel();
     JPanel bt_panel = new JPanel();
@@ -20,7 +19,6 @@ public class TicTacToeGame implements ActionListener {
     int chance_flag = 0;
     Random random = new Random();
     boolean pl1_chance;
-    
 
     TicTacToeGame() {
 
@@ -31,7 +29,6 @@ public class TicTacToeGame implements ActionListener {
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
 
-        
         textfield.setBackground(new Color(120, 20, 124));
         textfield.setForeground(new Color(25, 255, 0));
         textfield.setFont(new Font("Ink Free", Font.BOLD, 75));
@@ -52,7 +49,7 @@ public class TicTacToeGame implements ActionListener {
             bton[i].setFocusable(false);
             bton[i].addActionListener(this);
         }
-        
+
         t_panel.add(textfield);
         frame.add(t_panel, BorderLayout.NORTH);
         frame.add(bt_panel);
@@ -62,89 +59,84 @@ public class TicTacToeGame implements ActionListener {
 
     public void startGame() {
 
-        try {
-            textfield.setText("Loading....");
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
+        // try {
+        // textfield.setText("Loading....");
+        // Thread.sleep(4000);
+        // } catch (InterruptedException e) {
 
-            e.printStackTrace();
-        }
-        int chance=random.nextInt(100);
+        // e.printStackTrace();
+        // }
+        // int chance=random.nextInt(100);
 
-        if (chance%2 == 0) {
-            pl1_chance = true;
-            textfield.setText("X turn");
-        } else {
-            pl1_chance = false;
-            textfield.setText("O turn");
+        // if (chance%2 == 0) {
+        // pl1_chance = true;
+        // textfield.setText("X turn");
+        // } else {
+        // pl1_chance = false;
+        // textfield.setText("O turn");
+        // }
+        for (int i = 0; i < 9; i++) {
+            bton[i].setText("");
+            bton[i].setBackground(new JButton().getBackground());
+            bton[i].setEnabled(true);
         }
+        pl1_chance = true;
+        textfield.setText("X turn");
     }
-    public void gameOver(String s){
+
+    public void gameOver(String s) {
         chance_flag = 0;
-        Object[] option={"Restart","Exit"};
-        int n=JOptionPane.showOptionDialog(frame, "Game Over\n"+s,"Game Over",JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null,option,option[0]);
-        if(n==0){
+        Object[] option = { "Restart", "Exit" };
+        int n = JOptionPane.showOptionDialog(frame, "Game Over\n" + s, "Game Over", JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, option, option[0]);
+        if (n == 0) {
+            // frame.dispose();
+            // new TicTacToeGame();
+            startGame();
+        } else {
             frame.dispose();
-            new TicTacToeGame();
         }
-        else{
-            frame.dispose();
-        }
-    
+
     }
 
     public void matchCheck() {
         if ((bton[0].getText() == "X") && (bton[1].getText() == "X") && (bton[2].getText() == "X")) {
             xWins(0, 1, 2);
-        }
-        else if ((bton[0].getText() == "X") && (bton[4].getText() == "X") && (bton[8].getText() == "X")) {
+        } else if ((bton[0].getText() == "X") && (bton[4].getText() == "X") && (bton[8].getText() == "X")) {
             xWins(0, 4, 8);
-        }
-        else if ((bton[0].getText() == "X") && (bton[3].getText() == "X") && (bton[6].getText() == "X")) {
+        } else if ((bton[0].getText() == "X") && (bton[3].getText() == "X") && (bton[6].getText() == "X")) {
             xWins(0, 3, 6);
-        }
-        else if ((bton[1].getText() == "X") && (bton[4].getText() == "X") && (bton[7].getText() == "X")) {
+        } else if ((bton[1].getText() == "X") && (bton[4].getText() == "X") && (bton[7].getText() == "X")) {
             xWins(1, 4, 7);
-        }
-        else if ((bton[2].getText() == "X") && (bton[4].getText() == "X") && (bton[6].getText() == "X")) {
+        } else if ((bton[2].getText() == "X") && (bton[4].getText() == "X") && (bton[6].getText() == "X")) {
             xWins(2, 4, 6);
-        }
-        else if ((bton[2].getText() == "X") && (bton[5].getText() == "X") && (bton[8].getText() == "X")) {
+        } else if ((bton[2].getText() == "X") && (bton[5].getText() == "X") && (bton[8].getText() == "X")) {
             xWins(2, 5, 8);
-        }
-       else if ((bton[3].getText() == "X") && (bton[4].getText() == "X") && (bton[5].getText() == "X")) {
+        } else if ((bton[3].getText() == "X") && (bton[4].getText() == "X") && (bton[5].getText() == "X")) {
             xWins(3, 4, 5);
-        }
-       else if ((bton[6].getText() == "X") && (bton[7].getText() == "X") && (bton[8].getText() == "X")) {
+        } else if ((bton[6].getText() == "X") && (bton[7].getText() == "X") && (bton[8].getText() == "X")) {
             xWins(6, 7, 8);
         }
-      
+
         else if ((bton[0].getText() == "O") && (bton[1].getText() == "O") && (bton[2].getText() == "O")) {
             oWins(0, 1, 2);
-        }
-        else if ((bton[0].getText() == "O") && (bton[3].getText() == "O") && (bton[6].getText() == "O")) {
+        } else if ((bton[0].getText() == "O") && (bton[3].getText() == "O") && (bton[6].getText() == "O")) {
             oWins(0, 3, 6);
-        }
-        else if ((bton[0].getText() == "O") && (bton[4].getText() == "O") && (bton[8].getText() == "O")) {
+        } else if ((bton[0].getText() == "O") && (bton[4].getText() == "O") && (bton[8].getText() == "O")) {
             oWins(0, 4, 8);
-        }
-        else if ((bton[1].getText() == "O") && (bton[4].getText() == "O") && (bton[7].getText() == "O")) {
+        } else if ((bton[1].getText() == "O") && (bton[4].getText() == "O") && (bton[7].getText() == "O")) {
             oWins(1, 4, 7);
-        }
-        else if ((bton[2].getText() == "O") && (bton[4].getText() == "O") && (bton[6].getText() == "O")) {
+        } else if ((bton[2].getText() == "O") && (bton[4].getText() == "O") && (bton[6].getText() == "O")) {
             oWins(2, 4, 6);
-        }
-        else if ((bton[2].getText() == "O") && (bton[5].getText() == "O") && (bton[8].getText() == "O")) {
+        } else if ((bton[2].getText() == "O") && (bton[5].getText() == "O") && (bton[8].getText() == "O")) {
             oWins(2, 5, 8);
-        }
-        else if ((bton[3].getText() == "O") && (bton[4].getText() == "O") && (bton[5].getText() == "O")) {
+        } else if ((bton[3].getText() == "O") && (bton[4].getText() == "O") && (bton[5].getText() == "O")) {
             oWins(3, 4, 5);
         } else if ((bton[6].getText() == "O") && (bton[7].getText() == "O") && (bton[8].getText() == "O")) {
             oWins(6, 7, 8);
-        }
-        else if(chance_flag==9) {
+        } else if (chance_flag == 9) {
             textfield.setText("Match Tie");
-             gameOver("Match Tie");
+            gameOver("Match Tie");
         }
     }
 
@@ -171,7 +163,7 @@ public class TicTacToeGame implements ActionListener {
         textfield.setText("O Wins");
         gameOver("O Wins");
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
